8a3d824476a56704a2efa2fca294cfe5ce50d1d5
nguyentrong-phuc <nguyennguyen242242@gmail.com>
2026-07-11 02:59:38 +0700

    Flatten System Management navigation menu

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -51,9 +51,8 @@ const Layout = () => {
         <div className="sub-sidebar">
           <NavLink to="/system-management/time" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`}>System Time</NavLink>
           <NavLink to="/system-management/log" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`}>LOG</NavLink>
-          <div className="sidebar-item" style={{color: '#999'}}>System</div>
-          <NavLink to="/system-management/configuration" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} style={{paddingLeft: '30px'}}>Configuration Man...</NavLink>
-          <NavLink to="/system-management/reboot" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} style={{paddingLeft: '30px'}}>Reboot</NavLink>
+          <NavLink to="/system-management/configuration" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`}>Configuration Management</NavLink>
+          <NavLink to="/system-management/reboot" className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`}>Reboot</NavLink>
         </div>
       );
     }
