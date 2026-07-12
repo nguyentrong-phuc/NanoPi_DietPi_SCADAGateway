8c8f22bfadece7e045d31743664f879a280b39a5
nguyentrong-phuc <nguyennguyen242242@gmail.com>
2026-07-12 18:54:28 +0700

    fix: ReferenceError useEffect in SystemTime

--- frontend/src/pages/system/SystemTime.jsx
+++ frontend/src/pages/system/SystemTime.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DatePicker, TimePicker, ConfigProvider } from 'antd';
 import dayjs from 'dayjs';
 
